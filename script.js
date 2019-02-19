095e2b114a2fe699d46b0eaab8181bac806eaad4
khalidjambi <khalid.jambi18@gmail.com>
2019-02-19 14:44:52 +0700

    halo

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-document.getElementById('box1').innerHTML = "Sekolah koding";
-var boxes = document.getElementsByClassName('boxes');
-boxes [0].innerHTML = "versi terbaru 1";
-boxes [1].innerHTML = "versi sebelumnya 2";
-boxes [2].innerHTML = "versi terlama 3";
+// Dom selection
 
-var list = document.getElementsByTagName('li');
-list [0].innerHTML = "PHP";
-list [1].innerHTML = "SASS";
-list [2].innerHTML = "jQuery";
+// document.getElementById() -->element
+
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'Khalid Ibrahim';
+
+// document.getElementsByTagName() -> Html Colection
+
+const p = document.getElementsByTagName ('p');
+
+for( let i = 0; i < p.length; i++ ) {
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+
+// document.getElemtsByClassName()
+//--> HTML collection
+
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Ini diubah dari Javascript';
