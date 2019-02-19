79487434b25e363776e29cd02d21abfd331b4420
khalidjambi <khalid.jambi18@gmail.com>
2019-02-19 08:37:40 +0700

    bos

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-//  Dom selection
+document.getElementById('box1').innerHTML = "Sekolah koding";
+var boxes = document.getElementsByClassName('boxes');
+boxes [0].innerHTML = "versi terbaru 1";
+boxes [1].innerHTML = "versi sebelumnya 2";
+boxes [2].innerHTML = "versi terlama 3";
 
-//document.getElementById() -> element
-
-const judul = document.getElementById('judul');
-judul.style.color = 'red';
-judul.style.backgroundColor = 'grey';
-judul.innerHTML = 'Tuan Gober';
-
-
-// document.getElementsByTagName()-> HTML collections
-// -> HTML collection
-const p = document.getElementsByTagName ('p');
-
-for ( let i = 0; i < p.length; i++ ) {
-    p[i].style.backgroundColor = 'lightblue';
-}
-
-const h1 = document.getElementsByTagName('h1')[0];
-h1.style.fontSize = '50px';
-
-// document.getElementsByClassName()
-// -> HTML collection
-
-const p1 = document.getElementsByClassName('p1')[0];
-p1.innerHTML = 'Ini diubah dari Javasricpt'
+var list = document.getElementsByTagName('li');
+list [0].innerHTML = "PHP";
+list [1].innerHTML = "SASS";
+list [2].innerHTML = "jQuery";
